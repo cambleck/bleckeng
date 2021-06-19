b97de6028d606f434d8e32bf1381397be3ebae67
cambleck <bleckcam@gmail.com>
2021-06-18 23:45:48 -0500

    sidenav

--- client/src/pages/Landing/Sidenav/Body/index.js
+++ client/src/pages/Landing/Sidenav/Body/index.js
@@ -1,70 +1,27 @@
 import React from "react";
 import _ from "lodash";
 import { Link } from "react-router-dom";
-import { menuList, pinnedList } from "./itemsList";
-import MoreDropdown from "./MoreDropdown";
+import { menuList } from "./itemsList";
 import "./index.css";
-
-const ShowMoreButton = ({ icon, label }) => {
-  return (
-    <div
-      className="sidenav-button dropdown-trigger"
-      href="./"
-      data-target="dropdown2"
-      style={{
-        display: "flex",
-        alignItems: "center",
-        paddingLeft: 25,
-        width: "100%",
-      }}
-    >
-      <i
-        className="material-icons small"
-        style={{ marginRight: icon !== "" && 10 }}
-      >
-        {icon}
-      </i>
-      <span id="sideNav-label" style={{ textAlign: "left" }}>
-        {label}
-      </span>
-
-      <MoreDropdown />
-    </div>
-  );
-};
 
 const MenuList = ({ list, onClick, display }) => {
   console.log(display);
   return _.map(list, ({ icon, label, pageType }) => {
     return (
-      <>
-        {pageType === "more" ? (
-          <ShowMoreButton icon={icon} label={label} />
-        ) : (
-          <Link
-            className="sidenav-button sidenav-close"
-            style={{
-              display: "flex",
-              alignItems: "center",
-              color: display === pageType && "#00B797",
-              paddingLeft: 25,
-            }}
-            to={`/${pageType.toLowerCase()}`}
-            key={label}
-            onClick={() => onClick(pageType ? pageType : "")}
-          >
-            <i
-              className="material-icons small"
-              style={{ marginRight: icon !== "" && 10 }}
-            >
-              {icon}
-            </i>
-            <span id="sideNav-label" style={{ textAlign: "left" }}>
-              {label}
-            </span>
-          </Link>
-        )}
-      </>
+      <Link
+        className="sidenav-button sidenav-close"
+        style={{
+          display: "flex",
+          alignItems: "center",
+          color: display === pageType && "#00B797",
+          paddingLeft: 35,
+        }}
+        to={`/${pageType.toLowerCase()}`}
+        key={label}
+        onClick={() => onClick(pageType ? pageType : "")}
+      >
+        {pageType}
+      </Link>
     );
   });
 };
