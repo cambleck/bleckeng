61b919ac86164c5a20dafa62061770741742bb10
cambleck <bleckcam@gmail.com>
2021-07-04 14:47:04 -0500

    Home

--- client/src/pages/Landing/Home.js
+++ client/src/pages/Landing/Home.js
@@ -1,7 +1,49 @@
 import React from "react";
+import image from "../../images/beach.jpeg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        height: "100vh",
+        backgroundImage: `url(${image})`,
+        backgroundSize: "cover",
+        flexDirection: "column",
+        position: "relative",
+
+        alignItems: "center",
+        justifyContent: "center",
+        color: "white",
+      }}
+    >
+      <div
+        style={{
+          border: "2px solid rgb(200,200,200)",
+          width: "75%",
+          height: "75%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: 3,
+          fontWeight: "bold",
+          textShadow: "0px 0px 20px black",
+          fontSize: 24,
+          flexDirection: "column",
+          boxShadow: "0px 0px 20px gray",
+          textAlign: "center",
+        }}
+      >
+        <div style={{ marginBottom: 10, marginTop: -20 }}>
+          BLECK ENGINEERING
+        </div>
+        <div style={{ fontSize: 16, fontWeight: "normal" }}>
+          Providing context sensitive solutions since 1945
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
