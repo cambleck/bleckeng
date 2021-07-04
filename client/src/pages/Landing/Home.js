77c6907064749631fea9eab03536efcc60d8e4ca
cambleck <bleckcam@gmail.com>
2021-07-04 15:24:55 -0500

    Home

--- client/src/pages/Landing/Home.js
+++ client/src/pages/Landing/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import image from "../../images/beach.jpeg";
+import { FaAngleRight, FaAngleLeft } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -24,22 +25,47 @@ const Home = () => {
           width: "75%",
           height: "75%",
           display: "flex",
-          alignItems: "center",
           justifyContent: "center",
+          alignItems: "center",
           borderRadius: 3,
           fontWeight: "bold",
           textShadow: "0px 0px 20px black",
           fontSize: 24,
-          flexDirection: "column",
+
           boxShadow: "0px 0px 20px gray",
           textAlign: "center",
+          position: "relative",
         }}
       >
-        <div style={{ marginBottom: 10, marginTop: -20 }}>
-          BLECK ENGINEERING
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
+          <button className="clear-button" style={{ marginLeft: -40 }}>
+            <FaAngleLeft />
+          </button>
+          <div style={{ margin: 20 }}>
+            <div style={{ marginBottom: 10, marginTop: -20 }}>
+              BLECK ENGINEERING
+            </div>
+            <div style={{ fontSize: 16, fontWeight: "normal" }}>
+              Providing context sensitive solutions since 1945
+            </div>
+            <button className="contact-button">CONTACT</button>
+          </div>
+          <button className="clear-button" style={{ marginRight: -40 }}>
+            <FaAngleRight />
+          </button>
         </div>
-        <div style={{ fontSize: 16, fontWeight: "normal" }}>
-          Providing context sensitive solutions since 1945
+        <div style={{ display: "flex", position: "absolute", bottom: 20 }}>
+          <div>●</div>
+          <div>◦</div>
+          <div>◦</div>
+          <div>◦</div>
         </div>
       </div>
     </div>
